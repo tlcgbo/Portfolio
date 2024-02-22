fdf266901f50d8b17a562549df97c2b94ebe2422
Igbagbo <iolufiranye@gmail.com>
2024-02-22 18:53:01 +0000

    updating files in pages folder

---
+++ src/pages/Bookshelf.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Bookshelf = () => {
+  return (
+    <div>
+        <h1>Bookshelf</h1>
+    </div>
+  )
+}
+
+export default Bookshelf
---
+++ src/pages/Stack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stack = () => {
+  return (
+    <div>
+        <h1>Stack</h1>
+    </div>
+  )
+}
+
+export default Stack
