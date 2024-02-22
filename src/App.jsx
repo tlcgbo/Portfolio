717aa6658c54fb37d95557511ef5517404627e86
Igbagbo <iolufiranye@gmail.com>
2024-02-22 17:13:53 +0000

    pushing portfolio website to github

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold underline'>Hello World</h2>
+      <h2>This is Igbagbo</h2>
+    </div>
+  )
+}
+
+export default App
+
