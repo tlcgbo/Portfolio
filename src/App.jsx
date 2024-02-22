c9728a6218b2e52a1bdf4702b5825828aee40898
Igbagbo <iolufiranye@gmail.com>
2024-02-22 18:20:06 +0000

    pushing portfolio to github

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Footer from './component/Footer'
 
 import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <h2 className='text-3xl font-bold underline'>Hello World</h2>
-      <h2>This is Igbagbo</h2>
+      <Footer />
     </div>
+    
   )
 }
 
---
+++ src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default Footer
