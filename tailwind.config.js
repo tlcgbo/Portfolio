cad76c1371eeb04666573623d6ef132b5de1403b
Igbagbo <iolufiranye@gmail.com>
2024-02-22 18:38:49 +0000

    updated tailwind.css config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        tertiary: "#151030",
+      "black-100": "#100d25",
+      "black-200": "#090325",
+      "white-100": "#f3f3f3",
+
+      },
+      fontFamily: {
+        'lovers': ['Lovers Quarrel', 'cursive'],
+        'saint': ['Mrs Saint Delafield', 'cursive'],
+      },
+      screens: {
+        xs: "450px",
+        sm: "640px",
+        md: "710px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1536px",
+      }
+    },
   },
   plugins: [],
 }
