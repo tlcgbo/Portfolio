2cf56e47131b0c0457a637bed5d10b5fc2ac3d50
Igbagbo <iolufiranye@gmail.com>
2024-02-29 18:55:40 +0000

    updating more files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import NavBar from './component/NavBar'
+import Homepage from './pages/Homepage'
+import About from './pages/About'
 
-import React from 'react'
+import Stack from './pages/Stack'
+import Contact from './pages/Contact'
+import Bookshelf from './pages/Bookshelf'
 
 const App = () => {
   return (
-    <div>
-      <NavBar />
+    <Router>
+
+    <div className=' py-4 flex items-center justify-center'>
+      <div className='main w-[95vw] bg-tertiary rounded-3xl'>
+        <NavBar />
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/stack' element={<Stack />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/bookshelf' element={<Bookshelf />} />
+          </Routes>
+          
+      </div>
     </div>
+
+      
+    </Router>
     
   )
 }
--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -11,7 +11,31 @@ const NavBar = () => {
       <nav className='nav-full sticky top-0 left-[4.5vw] rounded-b-2xl lg:w flex justify-center pt-5 z-10'>
         <div className='nav-container flex justify-between w-[65%] mx-auto'>
 
-        <Logo />
+          <Logo />
+
+          <ul className='flex justify-around w-[9cm] pt-2 text-[15px] items-center h-12'>
+            <li>
+              <Link to="/about" className='cursor-pointer'>
+                About
+              </Link>
+            </li>
+            <li className='nav-full-li'>
+              <Link to="/#projects" className='cursor-pointer' >
+                Projects
+              </Link>
+            </li>
+            <li>
+            <Link to="/stack" className='cursor-pointer'>
+              Stack
+            </Link>
+            </li>
+            <li>
+            <Link to="contact" className='cursor-pointer'>
+                Contact
+              </Link>
+          </li>
+          </ul>
+
         </div>
       </nav>
     </>
